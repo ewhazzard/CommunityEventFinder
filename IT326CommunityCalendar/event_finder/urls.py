576699d0c683de48a359a0085b191767028017ba
Evan  Hazard <evanhazard@Evans-MacBook-Pro.local>
2022-10-15 16:59:06 -0500

    Added bootstrap functionality

--- IT326CommunityCalendar/event_finder/urls.py
+++ IT326CommunityCalendar/event_finder/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='homepage')
+    path('', views.home, name='homepage'),
+    path('admin/', admin.site.urls)
 ]
