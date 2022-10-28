19055d5abc199b41cb16cdbe6049e8b455084216
amloca1 <amloca1@ash.ad.ilstu.edu>
2022-10-27 22:35:34 -0500

    added event details and more to event.py

--- IT326CommunityCalendar/Utils/event.py
+++ IT326CommunityCalendar/Utils/event.py
@@ -3,4 +3,15 @@ class Event:
         self.__event_id=event_id
         self.__contact_info=contact_info
         self.__user_id=user_id
-        
+        self.comments=[]
+        self.RSVPs=[]
+    
+    def view_details(self,):
+
+    def add_RSVP(self,id):
+
+    def add_comment(self,id):
+
+    def remove_RSVP(self,id):
+
+    def remove_comment(self,id): 
