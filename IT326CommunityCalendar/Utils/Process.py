ce165ad9ea66858f2030e473459b6fdb8f9d0006
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 23:02:06 -0500

    Added Comment.py, Process.py, RSVP.py

---
+++ IT326CommunityCalendar/Utils/Process.py
@@ -0,0 +1,48 @@
+class Process:
+    def __init__(self):
+
+        # Variables
+        process_id
+        db_obj
+        process_content
+
+        # Methods
+
+        def Process(process_id, process_content):
+            pass
+
+        def create_comment():
+            pass
+
+        def create_event():
+            pass
+
+        def delete_event():
+            pass
+
+        def RSVP():
+            pass
+
+        def get_process_id():
+            pass
+
+        def set_process_id(process_id):
+            pass
+
+        def get_process_content():
+            pass
+
+        def set_process_content(process_content):
+            pass
+
+        def save(Event):
+            pass
+
+        def save(User):
+            pass
+
+        def delete_user(User):
+            pass
+
+        def dbConnect():
+            pass
---
+++ IT326CommunityCalendar/Utils/RSVP.py
@@ -0,0 +1,18 @@
+class RSVP:
+    def __init__(self):
+
+        # Variables
+
+        RSVP_date
+        user_id
+
+        # Methods
+
+        def RSVP(Date):
+            pass
+
+        def get_RSVP_date():
+            pass
+
+        def get_user_id():
+            pass
