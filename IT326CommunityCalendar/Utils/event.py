d781d128b2eb562cf1f791c5f2d9a4fa0fc276e1
amloca1 <amloca1@ash.ad.ilstu.edu>
2022-10-27 21:52:14 -0500

    added utils folder

---
+++ IT326CommunityCalendar/Utils/event.py
@@ -0,0 +1,6 @@
+class Event:
+    def __init__(self,event_id,contact_info,user_id):
+        self.__event_id=event_id
+        self.__contact_info=contact_info
+        self.__user_id=user_id
+        
