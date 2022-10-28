7dcb35f3b42948eccb12564c6c011005b21edaa8
Godzilla97 <lando.97.97@gmail.com>
2022-10-27 22:10:33 -0500

    Added comment and RSVP files

---
+++ IT326CommunityCalendar/Utils/comment.py
@@ -0,0 +1,8 @@
+class Comment:
+    def __init__(self, content, comment_id, comment_date, user_id):
+        self._content = content
+        self._comment_id = comment_id
+        self._comment_date = comment_date
+        self._user_id = user_id
+    
+    
