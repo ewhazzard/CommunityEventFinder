d906ca8e92891f00f6037709c4f0c2f341cb0683
Evan Hazzard <ewhazzard@gmail.com>
2022-11-15 19:25:54 -0600

    Added MySQL Database connection

--- IT326CommunityCalendar/IT326CommunityCalendar/settings.py
+++ IT326CommunityCalendar/IT326CommunityCalendar/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'IT326CommunityCalendar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CommunityEventFinderDB',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
