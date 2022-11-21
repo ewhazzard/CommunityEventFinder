b2c00059589b9ee59e185084660718b4087900d1
amloca1 <amloca1@ash.ad.ilstu.edu>
2022-11-21 14:54:49 -0600

    added framework for functions in process class

--- IT326CommunityCalendar/Utils/Process.py
+++ IT326CommunityCalendar/Utils/Process.py
@@ -1,48 +1,29 @@
 class Process:
     def __init__(self):
-
+        pass
         # Variables
-        process_id
-        db_obj
-        process_content
+        #process_id
+        #db_obj
+        #process_content
 
         # Methods
 
-        def Process(process_id, process_content):
-            pass
 
-        def create_comment():
-            pass
 
-        def create_event():
-            pass
+    def delete_event(self,event):
+        pass
 
-        def delete_event():
-            pass
+    def save(self,event):
+        pass
 
-        def RSVP():
-            pass
+    def save(self,user):
+        pass
 
-        def get_process_id():
-            pass
+    def delete_user(self,user):
+        pass
 
-        def set_process_id(process_id):
-            pass
+    def retriveEvent(self,event_id):
+        pass
 
-        def get_process_content():
-            pass
-
-        def set_process_content(process_content):
-            pass
-
-        def save(Event):
-            pass
-
-        def save(User):
-            pass
-
-        def delete_user(User):
-            pass
-
-        def dbConnect():
-            pass
+    def retriveUser(self,user_id):
+        pass
