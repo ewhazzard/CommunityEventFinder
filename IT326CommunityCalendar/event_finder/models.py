d595cb98224bd275baa1b165a0c343e5766e8185
beebelucas <beebelucas@gmail.com>
2022-11-29 13:50:04 -0600

    Fixed format

--- IT326CommunityCalendar/event_finder/models.py
+++ IT326CommunityCalendar/event_finder/models.py
@@ -25,8 +25,9 @@ class Users(models.Model):
     user_state = models.CharField(verbose_name="Location: State", max_length=100)
     user_admin = models.BooleanField(verbose_name="Is Admin")
 
-class Event(models.Model):
 
+class Event(models.Model):
+    
     event_id = models.IntegerField(verbose_name="Event ID", primary_key=True)
     user_id = models.IntegerField(verbose_name="Event ID", foreign_key=True)
     event_name = models.CharField(verbose_name="Event Name", max_length=50)
