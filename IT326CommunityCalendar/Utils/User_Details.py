e81c15a2dab877289c40ad846e4ba3f5ef8a1116
Evan Hazzard <evanhazzard@Evans-MacBook-Pro.local>
2022-12-02 01:10:53 -0600

    Add instance variables and revised constructors

--- IT326CommunityCalendar/Utils/User_Details.py
+++ IT326CommunityCalendar/Utils/User_Details.py
@@ -1,10 +1,20 @@
 
 class User_Details:
-    def __init__ (self,hobbies,age,intrests,gender):
-        self.hobbies=hobbies
-        self.age=age
-        self.intrests=intrests
-        self.gender=gender
+    
+    # List of strings. Each element is a hobby
+    hobbies = []
+    # List of strings. Each element is an interest
+    interests = []
+    # Integer age of the user
+    age = 0
+    # String representation of Gender
+    gender = ''
+    
+    def __init__ (self,hobbies,intrests,age, gender):
+        self.hobbies = hobbies
+        self.intrests = intrests
+        self.age = age
+        self.gender = gender
         
     
     def get_hobbies(self):
@@ -19,8 +29,8 @@ class User_Details:
     def get_gender(self):
         return self.gender
 
-    def set_hobbies(self,hobbies):
-        pass
+    def add_hobbies(self, hobbies_to_add):
+        self.hobbies.extend(hobbies_to_add)
 
     def set_age(self,age):
         self.age=age
