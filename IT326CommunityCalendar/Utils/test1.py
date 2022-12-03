31eca3c6a8b1528b27fba03a89296ab43688b9df
beebelucas <beebelucas@gmail.com>
2022-12-03 17:49:32 -0600

    Test Changes

--- IT326CommunityCalendar/Utils/test1.py
+++ IT326CommunityCalendar/Utils/test1.py
@@ -1,5 +1,10 @@
 import pytest
 
+import os
+print(os.getcwd())
+
+print(os.getcwd())
+
 from event import Event
 from Contact_Info import Contact_Info
 from event_details import Event_Details
@@ -11,8 +16,15 @@ def test_example():
 def test_example2():
     assert 2 == 2
 
-def test_event_location_creation_retrieval():
+def test_event_contact_info_creation_retrieval():
     testContact = Contact_Info("Dan","Smith","dsmith@ilstu.edu","318-329-2930")
 
     testEvent = Event("1111",testContact,"2222")
     assert testEvent.get_contact_info().get_first_name() == testContact.get_first_name()
+    assert testEvent.get_contact_info().get_last_name() == testContact.get_last_name()
+    assert testEvent.get_contact_info().get_email() == testContact.get_email()
+    assert testEvent.get_contact_info().get_phone() == testContact.get_phone()
+
+def test_event_location_info_creation_retrieval():
+    pass
+
