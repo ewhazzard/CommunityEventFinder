dc75a3b7571774d0fa7bb9f08bb2ec95c47f9221
Evan Hazzard <evanhazzard@Evans-MacBook-Pro.local>
2022-12-04 20:17:22 -0600

    Added update Event functionality

--- IT326CommunityCalendar/event_finder/urls.py
+++ IT326CommunityCalendar/event_finder/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profilepage'),
     path('eventcreate/',views.eventcreate,name='eventcreatepage'),
     path('createaccount/', views.create_account, name='createaccount'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('<int:event_id>/', views.update_event, name="updateevent")
 ]
