d8b891e1591b24945b17dad00d4b33568c617043
Godzilla97 <lando.97.97@gmail.com>
2022-12-06 01:37:21 -0600

    commentsPlease enter the commit message for your changes. Lines starting

--- IT326CommunityCalendar/Utils/RSVP.py
+++ IT326CommunityCalendar/Utils/RSVP.py
@@ -1,12 +1,15 @@
 class RSVP:
     def __init__(self, RSVP_date, user_id):
+        """Class constructor. """
         self._RSVP_date = RSVP_date
         self._user_id = user_id
 
     # def RSVP(user_id, RSVP_date):
 
     def get_RSVP_date(self):
+        """Getter for the RSVP date variable. """
         return self._RSVP_date
 
     def get_user_id(self):
+        """ Getter for the user id variable. """
         return self._user_id
