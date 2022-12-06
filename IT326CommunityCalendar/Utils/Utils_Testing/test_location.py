ca202bb7c5d351d0411ff62f0ded7ee5eba00ea0
beebelucas <beebelucas@gmail.com>
2022-12-06 02:19:11 -0600

    test_location fix

--- IT326CommunityCalendar/Utils/Utils_Testing/test_location.py
+++ IT326CommunityCalendar/Utils/Utils_Testing/test_location.py
@@ -8,4 +8,4 @@ def test_location_creation():
     assert testLocation.get_street() == "123 Easy Street"
     assert testLocation.get_city() == "Chicago"
     assert testLocation.get_state() == "Illinois"
-    assert testLocation.get_zipcode == 60123
+    assert testLocation.get_zipcode() == 60123
